5746662e6d2379459c3702b341ac8183177ff7f1
Zhong Yang <zhongyang219@hotmail.com>
2020-09-02 16:09:29 +0800

    update

--- taglib/toolkit/tstring.cpp
+++ taglib/toolkit/tstring.cpp
@@ -48,23 +48,42 @@ namespace
       return String::UTF16BE;
   }
 
+  bool isAscii(const char *s, size_t length)
+  {
+    for (int i = 0; i < length; ++i)
+    {
+      if (s[i] > 128)
+        return false;
+    }
+    return true;
+  }
+
   // Converts a Latin-1 string into UTF-16(without BOM/CPU byte order)
   // and copies it to the internal buffer.
   void copyFromLatin1(std::wstring &data, const char *s, size_t length)
   {
 #if (defined(_WIN32) || defined(_WIN64))
-    std::string str(s, length);
-    int size = MultiByteToWideChar(CP_ACP, 0, str.c_str(), -1, NULL, 0);
-    if (size <= 0)
+    if (isAscii(s, length))
     {
-      data.clear();
+      data.resize(length);
+      for (size_t i = 0; i < length; ++i)
+        data[i] = static_cast<unsigned char>(s[i]);
     }
     else
     {
-      wchar_t* str_unicode = new wchar_t[size + 1];
-      MultiByteToWideChar(CP_ACP, 0, str.c_str(), -1, str_unicode, size);
-      data.assign(str_unicode);
-      delete[] str_unicode;
+      std::string str(s, length);
+      int size = MultiByteToWideChar(CP_ACP, 0, str.c_str(), -1, NULL, 0);
+      if (size <= 0)
+      {
+        data.clear();
+      }
+      else
+      {
+        wchar_t* str_unicode = new wchar_t[size + 1];
+        MultiByteToWideChar(CP_ACP, 0, str.c_str(), -1, str_unicode, size);
+        data.assign(str_unicode);
+        delete[] str_unicode;
+      }
     }
 #else
     data.resize(length);
